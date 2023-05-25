750f204fda04eafb443de9fceb958fba04d1fc97
pedrohenrique <123123588+pedrohenriquebh@users.noreply.github.com>
2023-05-25 18:41:48 -0300

    Add files via upload

---
+++ C38_ProjectTemplate-main/C38_ProjectTemplate-main/js/Contestant.js
@@ -0,0 +1,37 @@
+class Contestant {
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var contestantCountRef = database.ref('contestantCount');
+
+
+    
+
+    contestantCountRef.on("value",(data)=>{
+      contestantCount = data();
+    })
+  }
+
+  updateCount(count){
+    database.ref("/").update({
+      contestantCount: count
+    });
+
+    
+    //database.ref("/").update({
+      //contestantCount: counts
+    //});
+
+    //database("/").update({
+      //contestantCount = count
+    //});
+  }
+
+ 
+
+ 
+}
